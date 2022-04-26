0629ea802f84f5707e7adc4475aa7763e41ec1ca
saharafathelbab <33489137+saharafathelbab@users.noreply.github.com>
2022-04-26 15:36:09 -0400

    33-search-in-rotated-sorted-array.js

---
+++ leetcode/1-Sequences/33-search-in-rotated-sorted-array.js
@@ -0,0 +1,12 @@
+//leetcode: https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    
+    return nums.indexOf(target);
+    
+};
